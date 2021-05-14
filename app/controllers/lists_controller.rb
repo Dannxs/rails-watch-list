3def235d6bbb625cee44a5efe2feed5230055b2e
Danny Cardoso <dcardoso066@gmail.com>
2021-05-14 17:28:15 +0200

    input photo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,7 +10,7 @@ class ListsController < ApplicationController
   end
   def create
     @list = List.new(list_params)
-    if @list.save
+    if @list.save!
       redirect_to list_path(@list)
     else
       render :new
@@ -20,6 +20,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
